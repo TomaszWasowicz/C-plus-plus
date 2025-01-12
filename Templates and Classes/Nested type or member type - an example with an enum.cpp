b6b9c382b18cfa6ae4d2a4cd7558b5360ec3fa2a
tomasz.wasowicz@gmail.com <tomasz.wasowicz@gmail.com>
2025-01-12 23:20:23 +0100

    12.01.2025 - nested/member types in classes

---
+++ Templates and Classes/Nested type or member type - an example with an enum.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class Fruit
+{
+public:
+	// FruitType has been moved inside the class, under the public access specifier
+		// It's renamed Type made an enum rather than an enum class
+	enum Type
+	{
+		apple,
+		banana,
+		cherry
+	};
+
+private:
+	Type m_type{};
+	int m_percentageEaten{ 0 };
+
+public:
+	Fruit(Type type) :
+		m_type{ type }
+	{
+	}
+
+	Type getType() { return m_type; }
+	int getPercentageEaten() { return m_percentageEaten; }
+
+	// Inside members of Fruit, it's not needed to have a prefix enumerators with FruitType::
+	bool isCherry() { return m_type == cherry; } 
+};
+
+int main()
+{
+	// Note: Outside the class, we have to access the enumerators via the Fruit:: prefix now
+	Fruit apple{ Fruit::apple };
+
+	if (apple.getType() == Fruit::apple)
+		std::cout << "I am an apple";
+	else
+		std::cout << "I am not an apple";
+
+	return 0;
+}
