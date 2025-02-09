fcdeb0e80ee5ec2a49cf3ed3b158dc2a73b89288
tomasz.wasowicz@gmail.com <tomasz.wasowicz@gmail.com>
2025-02-09 23:28:10 +0100

    09.02.2025 - exceptions - rethrowing

---
+++ Exceptions/Exceptions/Rethrowing an exception.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+class Base
+{
+public:
+    Base() {}
+    virtual void print() { std::cout << "Base"; }
+};
+
+class Derived : public Base
+{
+public:
+    Derived() {}
+    void print() override { std::cout << "Derived"; }
+};
+
+int main()
+{
+    try
+    {
+        try
+        {
+            throw Derived{};
+        }
+        catch (Base& b)
+        {
+            std::cout << "Caught Base b, which is actually a ";
+            b.print();
+            std::cout // now rethrowing the object here
+        }
+    }
+    catch (Base& b)
+    {
+        std::cout << "Caught Base b, which is actually a ";
+        b.print();
+        std::cout << '\n';
+    }
+
+    return 0;
+}
