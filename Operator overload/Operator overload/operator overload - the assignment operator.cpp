f51d4ccc5f3a457220a6baa1811277243fe25375
tomasz.wasowicz@gmail.com <tomasz.wasowicz@gmail.com>
2025-02-22 22:51:22 +0100

    20.02.2025 - operator overload - assignment operator

---
+++ Operator overload/Operator overload/operator overload - the assignment operator.cpp
@@ -0,0 +1,58 @@
+#include <cassert>
+#include <iostream>
+
+class Fraction
+{
+private:
+	int m_numerator{ 0 };
+	int m_denominator{ 1 };
+
+public:
+	// Default constructor
+	Fraction(int numerator = 0, int denominator = 1)
+		: m_numerator{ numerator }, m_denominator{ denominator }
+	{
+		assert(denominator != 0);
+	}
+
+	// Copy constructor
+	Fraction(const Fraction& copy)
+		: m_numerator{ copy.m_numerator }, m_denominator{ copy.m_denominator }
+	{
+		// no need to check for a denominator of 0 here since copy must already be a valid Fraction
+		std::cout << "Copy constructor called\n"; // just to prove it works
+	}
+
+	// Overloaded assignment
+	Fraction& operator= (const Fraction& fraction);
+
+	friend std::ostream& operator<<(std::ostream& out, const Fraction& f1);
+
+};
+
+std::ostream& operator<<(std::ostream& out, const Fraction& f1)
+{
+	out << f1.m_numerator << '/' << f1.m_denominator;
+	return out;
+}
+
+// A simplistic implementation of operator= (see better implementation below)
+Fraction& Fraction::operator= (const Fraction& fraction)
+{
+	// do the copy
+	m_numerator = fraction.m_numerator;
+	m_denominator = fraction.m_denominator;
+
+	// return the existing object so we can chain this operator
+	return *this;
+}
+
+int main()
+{
+	Fraction fiveThirds{ 5, 3 };
+	Fraction f;
+	f = fiveThirds; // calls overloaded assignment
+	std::cout << f;
+
+	return 0;
+}
